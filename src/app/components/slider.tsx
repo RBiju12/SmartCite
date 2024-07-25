ef175f8bb75da899a7becd11620f02a0d2b954d9
Rishan Biju <rishanbiju@gmail.com>
2024-07-25 16:20:37 -0400

    Added navbar, styled home

--- src/app/components/slider.tsx
+++ src/app/components/slider.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+'use client'
+import React, {useState, useEffect} from 'react'
+import Cookies from 'js-cookie'
+ 
+import Squash from 'hamburger-react'
+import Navbar from '../components/Navbar'
 
 export default function Slider()
 {
+  const [toggle, setToggle] = useState<boolean>(false)
+  const [username, setUsername] = useState<string>('')
+
+  useEffect(() => {
+     let cookie = setInterval(() => {
+       let data: any = Cookies.get('username')
+
+       if (data)
+       {
+          setUsername(data)
+       }
+     }, 3000)
+
+     return () => clearInterval(cookie)
+  }, [])
 
   return (
       <>
+        <Squash toggled={toggle} toggle={setToggle} />
+        {toggle ? <Navbar username={username ? username : null}/> : null}
       </>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1>Welcome to SmartCite</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:">
+        <h1 style={{color: 'purple'}}>Welcome to SmartCite</h1>
       </div>
     </main>
   );
